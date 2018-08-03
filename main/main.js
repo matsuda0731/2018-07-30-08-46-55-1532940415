a2028bb2e3c6a685952189ea68a5b90791623931
ZZ <58353408@qq.com>
2018-08-03 23:50:54 +0800

    ZZ

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
 module.exports = function main() {
   // Write your code here
+    function(str){
+        let j;
+        let sum=0;
+        for(j=0;j<str.length;j++){
+            sum += parseInt(str[j]);
+        }
+       console.log(sum);
 };
